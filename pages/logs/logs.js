301a181ef02cf0e4f4985e9eb06807167bb4289a
Howie.Zhang <howiezhang@augmentum.com.cn>
2017-01-22 15:45:34 +0800

    add pull down

--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -4,6 +4,28 @@ Page({
   data: {
     logs: []
   },
+  bindIconFinishTap: function (e) {
+    var newDefaultTask = wx.getStorageSync('defaultTask') || []
+    var newFinishTask = wx.getStorageSync('finishTask') || []
+    for (let i = 0; i < newFinishTask.length; i++) {
+      if (newFinishTask[i]['date'] == e.target.id) {
+        newDefaultTask.unshift(newFinishTask[i])
+        newFinishTask.splice(i, 1);
+        break;
+      }
+    }
+    wx.setStorage({
+      key: "defaultTask",
+      data: newDefaultTask
+    })
+    wx.setStorage({
+      key: "finishTask",
+      data: newFinishTask
+    })
+    this.setData({
+      logs: newFinishTask,
+    })
+  },
   bindFinishDeleteTap: function(e) {
    var newFinishTask = wx.getStorageSync('finishTask') || []
    for (let i = 0; i < newFinishTask.length; i++) {
